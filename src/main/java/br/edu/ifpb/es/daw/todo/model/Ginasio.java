7394c1b9131c0606478693f6abf47d9ecbb5e2ce
Pedro <pedrohsdgomes@gmail.com>
2026-06-15 20:16:08 -0300

    feat: implementar busca paginada e filtros dinâmicos para Pokemon

--- src/main/java/br/edu/ifpb/es/daw/todo/model/Ginasio.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/model/Ginasio.java
@@ -37,7 +37,7 @@ public class Ginasio {
     @Column(name = "Especialidade", nullable = false)
     private Tipos tipo;
 
-    ToOne
+    @OneToOne
     @JoinColumn(name = "time_id", nullable = false)
     private Time time;
 
